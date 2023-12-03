5c329a6650587bec06932f6ce9296de34cd59940
Pranav Bharadhwaj <pranavbharadhwaj4957@gmail.com>
2023-12-03 10:20:06 +0530

    Data Ingestion stage

---
+++ main.py
@@ -0,0 +1,12 @@
+from NLP_Project.logging import logger
+from NLP_Project.pipeline.s1_dataIng import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
