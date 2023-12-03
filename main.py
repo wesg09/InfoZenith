44e82804a52c9bf1ce728bc166b2de73a09b493c
Pranav Bharadhwaj <pranavbharadhwaj4957@gmail.com>
2023-12-03 10:37:43 +0530

    Data Validation stage

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from NLP_Project.logging import logger
 from NLP_Project.pipeline.s1_dataIng import DataIngestionTrainingPipeline
+from NLP_Project.pipeline.s2_dataval import DataValidationTrainingPipeline
+
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_validation = DataValidationTrainingPipeline()
+   data_validation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
